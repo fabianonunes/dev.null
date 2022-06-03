1cd3081ec7e6a2de20ffd9fe6ba805ab551c4dda
Fabiano Parente <fabiano@nunes.me>
2022-06-02 21:16:29 -0300

    exibe tempo da request

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
 	router.Any("/dev/null", func(c *gin.Context) {
+		start := time.Now()
 		written, err := io.Copy(io.Discard, c.Request.Body)
 		if err != nil {
 			c.String(http.StatusInternalServerError, err.Error())
 		}
-		c.String(http.StatusOK, "sent %d bytes to /dev/null", written)
+		elapsed := time.Since(start)
+		c.String(http.StatusOK, "🔥 sent %d bytes to /dev/null in %s", written, elapsed)
 	})
 	_ = router.Run(":8080")
 }
