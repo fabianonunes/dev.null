e893068bd1c41ba4e7c34bbbbfa12baff6b85e97
Fabiano Parente <fabiano@nunes.me>
2023-01-05 14:48:34 -0300

    entrypoint de dados aleatórios

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module dev.null
 
 go 1.18
 
-require github.com/gin-gonic/gin v1.8.0
+require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/gin-gonic/gin v1.8.0
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"crypto/rand"
+	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 	"time"
 )
+
+type RandomReader struct{}
+
+func (reader RandomReader) Read(b []byte) (int, error) {
+	size, err := rand.Read(b)
+	return size, err
+}
 
 func main() {
 	router := gin.Default()
@@ -17,6 +26,21 @@ func main() {
 		}
 		elapsed := time.Since(start)
 		c.String(http.StatusOK, "🔥 sent %d bytes to /dev/null in %s", written, elapsed)
+	})
+
+	router.Any("/dev/bytes/:size", func(c *gin.Context) {
+		input := c.Param("size")
+		size, err := humanize.ParseBytes(input)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		total := int64(size)
+		_, err = io.CopyN(c.Writer, RandomReader{}, total)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
 	})
 	_ = router.Run(":8080")
 }
